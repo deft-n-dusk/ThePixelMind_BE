6109e5587c5db5e89da02a8920d7b11b64a12c25
Sonal Tyagi <sonaltyagi.biz@gmail.com>
2025-08-29 16:23:14 +0530

    Added productSchema

---
+++ src/models/product.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const productSchema = mongoose.Schema({
+    title:{
+        type: String,
+        required: true,
+    },
+    price:{
+        type: Number,
+        required: true,
+        min: 1,
+    },
+    description:{
+        type: String,
+        required: true,
+    },
+    category:{
+        type: String,
+        required: true,
+    },
+    imageURL:{
+        type: String,
+        default: "https://baskiofisi.com.tr/wp-content/uploads/default-image-1.jpg",
+        validate(value){
+            if(!validator.isURL(value)){
+                throw new Error("imageURL is not valid")
+            }
+        },
+    },
+},
+{
+    timestamps: true,
+});
+
+productSchema.index({title: 1});
+
+const Product = mongoose.model("Product", productSchema);
+module.exports = Product;
